57c30b8532c42f70fb2f8687123f4527a1810ea0
Li Eric Beixu <1764646958@qq.com>
2026-05-30 21:42:28 +0800

    feat: implement Bilibili content adapter

--- backend/src/adapters/index.ts
+++ backend/src/adapters/index.ts
@@ -2,4 +2,5 @@ export * from './types';
 export * from './base-adapter';
 export * from './xiaohongshu-adapter';
 export * from './zhihu-adapter';
+export * from './bilibili-adapter';
 export * from './registry';
